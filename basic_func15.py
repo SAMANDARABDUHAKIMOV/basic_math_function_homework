b55080ab9b893d700c94d55b4ec82013308e6c17
samandar <samandar.abduhakimov.007@gmail.com>
2023-10-27 01:51:04 +0500

    nn

--- basic_func15.py
+++ basic_func15.py
@@ -1,11 +1,4 @@
-def main(a, b):
-    '''Find the remainder when a is divided by b and return it.
-    
-    Args:
-        a (int): a number
-        b (int): a number
-        
-    Returns:
-        int: the result.
-    '''
-    return
+a=23
+b=5
+c=a%b
+print(c)
